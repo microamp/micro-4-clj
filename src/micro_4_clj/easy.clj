593af497d8955e5d6f7ce9e59e47358c0f78346e
han semaj <sangho.nah@gmail.com>
2014-04-30 22:56:33 +1200

    'Recognize Playing Cards' solved

--- src/micro_4_clj/easy.clj
+++ src/micro_4_clj/easy.clj
@@ -461,3 +461,24 @@ Write a function which returns the nth row of Pascal's Triangle."
            false))
 (assert (= (btree? '(:a nil ()))
            false))
+
+"Recognize Playing Cards"
+"A standard American deck of playing cards has four suits - spades, hearts, diamonds, and clubs - and thirteen cards in each suit. Two is the lowest rank, followed by other integers up to ten; then the jack, queen, king, and ace.
+
+It's convenient for humans to represent these cards as suit/rank pairs, such as H5 or DQ: the heart five and diamond queen respectively. But these forms are not convenient for programmers, so to write a card game you need some way to parse an input string into meaningful components. For purposes of determining rank, we will define the cards to be valued from 0 (the two) to 12 (the ace)
+
+Write a function which converts (for example) the string \"SJ\" into a map of {:suit :spade, :rank 9}. A ten will always be represented with the single character \"T\", rather than the two characters \"10\"".
+(defn card [repr]
+  (let [suits {\S :spade \H :heart \D :diamond \C :club}
+        ranks {\T 8 \J 9 \Q 10 \K 11 \A 12}
+        suit (first repr)
+        rank (last repr)]
+    {:suit (get suits suit)
+     :rank (or (get ranks rank) (- (-> rank str Integer/parseInt int) 2))}))
+
+(= {:suit :diamond :rank 10} (card "DQ"))
+(= {:suit :heart :rank 3} (card "H5"))
+(= {:suit :club :rank 12} (card "CA"))
+(= (range 13) (map (comp :rank card str)
+                   '[S2 S3 S4 S5 S6 S7
+                     S8 S9 ST SJ SQ SK SA]))
