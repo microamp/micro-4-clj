16a914a91a0ebc1520459119be8985eb7140567b
han semaj <sangho.nah@gmail.com>
2014-05-10 18:07:00 +1200

    'Anagram Finder' solved

--- src/micro_4_clj/medium.clj
+++ src/micro_4_clj/medium.clj
@@ -229,3 +229,14 @@
                          (ibti (fn [a b]      ; both even or both odd
                                  (= (mod a 2) (mod b 2)))
                                :same)))))
+
+"Anagram Finder"
+"Write a function which finds all the anagrams in a vector of words. A word x is an anagram of word y if all the letters in x can be rearranged in a different order to form y. Your function should return a set of sets, where each sub-set is a group of words which are anagrams of each other. Each sub-set should have at least two words. Words without any anagrams should not be included in the result."
+(defn anagram [coll]
+  (set (map set
+            (filter #(> (count %) 1)
+                    (vals (group-by set coll))))))
+(assert (= (anagram ["meat" "mat" "team" "mate" "eat"])
+           #{#{"meat" "team" "mate"}}))
+(assert (= (anagram ["veer" "lake" "item" "kale" "mite" "ever"])
+           #{#{"veer" "ever"} #{"lake" "kale"} #{"mite" "item"}}))
