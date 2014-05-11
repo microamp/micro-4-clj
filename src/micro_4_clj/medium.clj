82d48f19b40e5da5c8023cb38df078e40ea17648
han semaj <sangho.nah@gmail.com>
2014-05-11 22:35:45 +1200

    'Intervals' solved

--- src/micro_4_clj/medium.clj
+++ src/micro_4_clj/medium.clj
@@ -286,3 +286,29 @@
 (assert (= {:a [1]} (identify-kv [:a 1])))
 (assert (= {:a [1], :b [2]} (identify-kv [:a 1, :b 2])))
 (assert (= {:a [1 2 3], :b [], :c [4]} (identify-kv [:a 1 2 3 :b :c 4])))
+
+"Intervals"
+"Write a function that takes a sequence of integers and returns a sequence of \"intervals\". Each interval is a a vector of two integers, start and end, such that all integers between start and end (inclusive) are contained in the input sequence."
+(defn intervals [c]
+  (if (empty? c)
+    []
+    (map (fn [v] [(first v) (last v)])
+         (let [sorted (-> c set sort)]
+           (reduce
+            (fn [v n]
+              (let [but-last (butlast v)
+                    last-v (last v)
+                    last-n (-> v last last)]
+                (if (= (inc last-n) n)
+                  (if (empty? but-last)
+                    [(conj last-v n)]
+                    (conj (vec but-last) (conj last-v n)))
+                  (conj v [n]))))
+            [[(first sorted)]]
+            (rest sorted))))))
+(assert (= (intervals [1 2 3]) [[1 3]]))
+(assert (= (intervals [10 9 8 1 2 3]) [[1 3] [8 10]]))
+(assert (= (intervals [1 1 1 1 1 1 1]) [[1 1]]))
+(assert (= (intervals []) []))
+(assert (= (intervals [19 4 17 1 3 10 2 13 13 2 16 4 2 15 13 9 6 14 2 11])
+           [[1 4] [6 6] [9 11] [13 17] [19 19]]))
