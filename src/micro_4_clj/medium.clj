0566ec92204480aadac331658990f3cb73c016a9
han semaj <sangho.nah@gmail.com>
2014-05-13 00:01:37 +1200

    'Perfect Numbers' solved

--- src/micro_4_clj/medium.clj
+++ src/micro_4_clj/medium.clj
@@ -339,3 +339,16 @@
 (assert (balanced? "([]([(()){()}(()(()))(([[]]({}()))())]((((()()))))))"))
 (assert (not (balanced? "([]([(()){()}(()(()))(([[]]({}([)))())]((((()()))))))")))
 (assert (not (balanced? "[")))
+
+"Perfect Numbers"
+"A number is \"perfect\" if the sum of its divisors equal the number itself. 6 is a perfect number because 1+2+3=6. Write a function which returns true for perfect numbers and false otherwise."
+(defn perfect? [n]
+  (let [divisors (filter #(= (mod n %) 0)
+                         (range 1 (-> n Math/sqrt int inc)))]
+    (= n (apply + (butlast (sort (mapcat #(vector % (/ n %))
+                                         divisors)))))))
+(assert (= (perfect? 6) true))
+(assert (= (perfect? 7) false))
+(assert (= (perfect? 496) true))
+(assert (= (perfect? 500) false))
+(assert (= (perfect? 8128) true))
