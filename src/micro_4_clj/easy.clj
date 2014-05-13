724590a81e29a3045906c0e89297e2771f4cc418
han semaj <sangho.nah@gmail.com>
2014-05-14 00:11:29 +1200

    refactoring

--- src/micro_4_clj/easy.clj
+++ src/micro_4_clj/easy.clj
@@ -1,24 +1,27 @@
 "Last Element"
 "Write a function which returns the last element in a sequence."
-(def x #(-> % reverse first))
-(assert (= (x [1 2 3 4 5]) 5))
-(assert (= (x '(5 4 3)) 3))
-(assert (= (x ["b" "c" "d"]) "d"))
+(defn my-last [c]
+  (-> c reverse first))
+(assert (= (my-last [1 2 3 4 5]) 5))
+(assert (= (my-last '(5 4 3)) 3))
+(assert (= (my-last ["b" "c" "d"]) "d"))
 
 "Penultimate Element"
 "Write a function which returns the second to last element from a sequence."
-(def x #(-> % reverse second))
-(assert (= (x (list 1 2 3 4 5)) 4))
-(assert (= (x ["a" "b" "c"]) "b"))
-(assert (= (x [[1 2] [3 4]]) [1 2]))
+(defn second-to-last [c]
+  (-> c reverse second))
+(assert (= (second-to-last (list 1 2 3 4 5)) 4))
+(assert (= (second-to-last ["a" "b" "c"]) "b"))
+(assert (= (second-to-last [[1 2] [3 4]]) [1 2]))
 
 "Duplicate a Sequence"
 "Write a function which duplicates each element of a sequence."
-(def x #(interleave % %))
-(assert (= (x [1 2 3]) '(1 1 2 2 3 3)))
-(assert (= (x [:a :a :b :b]) '(:a :a :a :a :b :b :b :b)))
-(assert (= (x [[1 2] [3 4]]) '([1 2] [1 2] [3 4] [3 4])))
-(assert (= (x [[1 2] [3 4]]) '([1 2] [1 2] [3 4] [3 4])))
+(defn duplicate-seq [c]
+  (interleave c c))
+(assert (= (duplicate-seq [1 2 3]) '(1 1 2 2 3 3)))
+(assert (= (duplicate-seq [:a :a :b :b]) '(:a :a :a :a :b :b :b :b)))
+(assert (= (duplicate-seq [[1 2] [3 4]]) '([1 2] [1 2] [3 4] [3 4])))
+(assert (= (duplicate-seq [[1 2] [3 4]]) '([1 2] [1 2] [3 4] [3 4])))
 
 "Nth Element"
 "Write a function which returns the Nth element from a sequence."
@@ -31,13 +34,13 @@
 
 "Count a Sequence"
 "Write a function which returns the total number of elements in a sequence."
-(defn cnt [coll]
-  (apply + (map (fn [_] 1) coll)))
-(assert (= (cnt '(1 2 3 3 1)) 5))
-(assert (= (cnt "Hello World") 11))
-(assert (= (cnt [[1 2] [3 4] [5 6]]) 3))
-(assert (= (cnt '(13)) 1))
-(assert (= (cnt '(:a :b :c)) 3))
+(defn my-count [c]
+  (reduce + (map (fn [_] 1) c)))
+(assert (= (my-count '(1 2 3 3 1)) 5))
+(assert (= (my-count "Hello World") 11))
+(assert (= (my-count [[1 2] [3 4] [5 6]]) 3))
+(assert (= (my-count '(13)) 1))
+(assert (= (my-count '(:a :b :c)) 3))
 
 "Sum It All Up"
 "Write a function which returns the sum of a sequence of numbers."
@@ -60,12 +63,10 @@
 
 "Reverse a Sequence"
 "Write a function which reverses a sequence."
-(defn my-reverse [coll]
-  (loop [current coll reversed []]
-    (if (empty? current)
-      reversed
-      (recur (butlast current)
-             (conj reversed (last current))))))
+(defn my-reverse [c]
+  (reduce (fn [reversed item] (cons item reversed))
+          (list (first c))
+          (rest c)))
 (assert (= (my-reverse [1 2 3 4 5]) [5 4 3 2 1]))
 (assert (= (my-reverse (sorted-set 5 7 2 7)) '(7 5 2)))
 (assert (= (my-reverse [[1 2] [3 4] [5 6]]) [[5 6] [3 4] [1 2]]))
@@ -73,8 +74,8 @@
 "Palindrome Detector"
 "Write a function which returns true if the given sequence is a palindrome.
 Hint: \"racecar\" does not equal '(\\r \\a \\c \\e \\c \\a \\r)"
-(defn palindrome? [sqnce]
-  (= (vec sqnce) (vec (reverse sqnce))))
+(defn palindrome? [c]
+  (= (apply str c) (apply str (reverse c))))
 (assert (false? (palindrome? '(1 2 3 4 5))))
 (assert (true? (palindrome? "racecar")))
 (assert (true? (palindrome? [:foo :bar :foo])))
@@ -103,7 +104,8 @@ Hint: \"racecar\" does not equal '(\\r \\a \\c \\e \\c \\a \\r)"
 "Get the Caps"
 "Write a function which takes a string and returns a new string containing only the capital letters."
 (defn caps-only [s]
-  (clojure.string/join (filter (fn [c] (Character/isUpperCase c)) s)))
+  (clojure.string/join
+   (filter (fn [c] (Character/isUpperCase c)) s)))
 (assert (= (caps-only "HeLlO, WoRlD!") "HLOWRD"))
 (assert (empty? (caps-only "nothing")))
 (assert (= (caps-only "$#A(*&987Zf") "AZ"))
@@ -118,10 +120,8 @@ dicate x) where x is an item in the collection."
 "Implement range"
 "Write a function which creates a list of all integers in a given range."
 (defn my-range [start end]
-  (loop [current start coll []]
-    (if (= current end)
-      coll
-      (recur (inc current) (conj coll current)))))
+  (take-while #(> end %)
+              (iterate inc start)))
 (assert (= (my-range 1 4) '(1 2 3)))
 (assert (= (my-range -2 2) '(-2 -1 0 1)))
 (assert (= (my-range 5 8) '(5 6 7)))
@@ -137,9 +137,9 @@ dicate x) where x is an item in the collection."
 
 "Interleave Two Seqs"
 "Write a function which takes two sequences and returns the first item from each, then the second item from each, then the third, etc."
-(defn my-interleave [coll1, coll2]
-  (let [len (min (count coll1) (count coll2))]
-    (reduce concat (map vector (take len coll1) (take len coll2)))))
+(defn my-interleave [c1 c2]
+  (let [len (min (count c1) (count c2))]
+    (mapcat vector (take len c1) (take len c2))))
 (assert (= (my-interleave [1 2 3] [:a :b :c]) '(1 :a 2 :b 3 :c)))
 (assert (= (my-interleave [1 2] [3 4 5 6]) '(1 3 2 4)))
 (assert (= (my-interleave [1 2 3 4] [5]) [1 5]))
@@ -147,10 +147,13 @@ dicate x) where x is an item in the collection."
 
 "Compress a Sequence"
 "Write a function which removes consecutive duplicates from a sequence."
-(defn compress [coll]
-  (map second
-       (filter (fn [[index item]] (not= item (get coll (dec index))))
-               (map vector (range) coll))))
+(defn compress [c]
+  (reduce (fn [compressed item]
+            (if (= (last compressed) item)
+              compressed
+              (conj compressed item)))
+          [(first c)]
+          (rest c)))
 (assert (= (apply str (compress "Leeeeeerrroyyy")) "Leroy"))
 (assert (= (compress [1 1 2 3 3 2 2 3]) '(1 2 3 2 3)))
 (assert (= (compress [[1 2] [1 2] [3 4] [1 2]]) '([1 2] [3 4] [1 2])))
@@ -175,9 +178,7 @@ dicate x) where x is an item in the collection."
 "Replicate a Sequence"
 "Write a function which replicates each element of a sequence a variable number of times."
 (defn replicate [coll n]
-  (if (> n 1)
-    (apply interleave (take n (repeat coll)))
-    coll))
+  (mapcat #(take n (repeat %)) coll))
 (assert (= (replicate [1 2 3] 2) '(1 1 2 2 3 3)))
 (assert (= (replicate [:a :b] 4) '(:a :a :a :a :b :b :b :b)))
 (assert (= (replicate [4 5 6] 1) '(4 5 6)))
@@ -211,9 +212,9 @@ dicate x) where x is an item in the collection."
 "Drop Every Nth Item"
 "Write a function which drops every Nth item from a sequence."
 (defn drop-every-nth [coll n]
-  (map second
-       (filter (fn [[idx val]] (not (zero? (mod (inc idx) n))))
-               (map vector (range) coll))))
+  (map #(get coll %)
+       (filter #(not (= (mod (inc %) n) 0))
+               (range (count coll)))))
 (assert (= (drop-every-nth [1 2 3 4 5 6 7 8] 3) [1 2 4 5 7 8]))
 (assert (= (drop-every-nth [:a :b :c :d :e :f] 2) [:a :c :e]))
 (assert (= (drop-every-nth [1 2 3 4 5 6] 4) [1 2 3 5 6]))
