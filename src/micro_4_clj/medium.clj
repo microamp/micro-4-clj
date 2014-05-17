4705cb6f08a43a60b1b406f8bf4783d6d6417871
han semaj <sangho.nah@gmail.com>
2014-05-18 00:51:21 +1200

    'Sequence of pronunciations' solved

--- src/micro_4_clj/medium.clj
+++ src/micro_4_clj/medium.clj
@@ -429,17 +429,17 @@ Write a function which accepts an integer n, a predicate p, and a sequence. It s
 "Write a function that returns a lazy sequence of \"pronunciations\" of a sequence of numbers. A pronunciation of each element in the sequence consists of the number of repeating identical numbers and the number itself. For example, [1 1] is pronounced as [2 1] (\"two ones\"), which in turn is pronounced as [1 2 1 1] (\"one two, one one\").
 
 Your function should accept an initial sequence of numbers, and return an infinite lazy sequence of pronunciations, each element being a pronunciation of the previous element."
-(defn pack [c]
-  (let [diffs (filter #(not (= (get c %)
-                               (get c (dec %))))
-                      (range 1 (count c)))]
-    (map (fn [[start end]] (subvec c start end))
-         (partition 2 1 (concat [0] diffs [(count c)])))))
 (defn pronunciations [coll]
-  (rest (iterate #(mapcat (fn [packed] [(count packed)
-                                       (first packed)])
-                          (pack (vec %)))
-                 coll)))
+  (let [pack (fn [c]
+               (let [diffs (filter #(not (= (get c %)
+                                            (get c (dec %))))
+                                   (range 1 (count c)))]
+                 (map (fn [[start end]] (subvec c start end))
+                      (partition 2 1 (concat [0] diffs [(count c)])))))]
+    (rest (iterate #(mapcat (fn [packed] [(count packed)
+                                         (first packed)])
+                            (pack (vec %)))
+                   coll))))
 (assert (= [[1 1] [2 1] [1 2 1 1]] (take 3 (pronunciations [1]))))
 (assert (= [3 1 2 4] (first (pronunciations [1 1 1 4 4]))))
 (assert (= [1 1 1 3 2 1 3 2 1 1] (nth (pronunciations [1]) 6)))
