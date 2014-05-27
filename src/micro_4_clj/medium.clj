ccbb069e9d3a5f886281bfc9facb694fa5872da0
han semaj <sangho.nah@gmail.com>
2014-05-27 19:32:39 +1200

    'Partition a Sequence' revisited

--- src/micro_4_clj/medium.clj
+++ src/micro_4_clj/medium.clj
@@ -47,11 +47,9 @@
 "Partition a Sequence"
 "Write a function which returns a sequence of lists of x items each. Lists of less than x items should not be returned."
 (defn my-partition [n coll]
-  (loop [left coll result []]
-    (let [temp (take n left)]
-      (if (< (count temp) n)
-        result
-        (recur (drop n left) (conj result temp))))))
+  (take-while #(= (count %) n)
+              (map #(take n (drop % coll))
+                   (iterate #(+ % n) 0))))
 (assert (= (my-partition 3 (range 9)) '((0 1 2) (3 4 5) (6 7 8))))
 (assert (= (my-partition 2 (range 8)) '((0 1) (2 3) (4 5) (6 7))))
 (assert (= (my-partition 3 (range 8)) '((0 1 2) (3 4 5))))
